1fa15ac45c255a0e4a97f3de985bb2a2585c1ea0
Luis Antonio Vázquez García <luis.vazquezga@udlap.mx>
2023-08-26 17:30:02 -0600

    undestanding pipes

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, ParseIntPipe } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { SlugPipe } from './slug/slug.pipe';
 
 @ApiTags('courses')
 @Controller('courses')
@@ -10,27 +11,14 @@ export class CoursesController {
   constructor(private readonly coursesService: CoursesService) {}
 
   @Post()
+  @HttpCode(201)
   create(@Body() createCourseDto: CreateCourseDto) {
     return this.coursesService.create(createCourseDto);
   }
 
-  @Get()
-  findAll() {
-    return this.coursesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.coursesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCourseDto: UpdateCourseDto) {
-    return this.coursesService.update(+id, updateCourseDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.coursesService.remove(+id);
+  @Get(':title')
+  getDetail(@Param('title', new SlugPipe()) title: number) {
+    console.log(title);
+    return this.coursesService.findOne(1);
   }
 }
