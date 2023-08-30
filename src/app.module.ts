6f24cb5c78838fff22d8ba11bba8b42619012c31
Luis Antonio Vázquez García <luis.vazquezga@udlap.mx>
2023-08-30 11:07:16 -0600

    connecting to mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,8 @@ import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { UsersModule } from './users/users.module';
+import { Mongoose } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -18,6 +20,7 @@ import { UsersModule } from './users/users.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    MongooseModule.forRoot(process.env.DB_URI),
     CoursesModule,
     AuthModule,
     VideosModule,
